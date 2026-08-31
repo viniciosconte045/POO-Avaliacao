7ead6c36662cf4e015ae87fd4f885afb66b269fe
Vinicios Alves <viniciosconte045@gmail.com>
2026-08-31 15:11:06 -0300

    pq tem tantos commits

---
+++ Focas.cs
@@ -0,0 +1,73 @@
+public class Foca
+{
+    private string? nome;
+
+    private int idade;
+
+    private string? especie;
+
+    private double altura;
+
+    private double peso;
+
+    public string? Nome
+    {
+        get { return nome;}
+        set { nome = value;}
+    }
+
+    public int Idade
+    {
+        get {return idade;}
+        set 
+        { 
+            if (value >=0)
+                idade = value;
+
+        }
+    }
+
+    public string? Especie
+    {
+        get { return especie; }
+        set { especie = value; }
+    }
+
+        public double Altura
+    {
+        get { return altura; }
+        set
+        {
+            if (value > 0)
+                altura = value;
+        }
+    }
+
+    public double Peso
+    {
+        get { return peso; }
+        set
+        {
+            if (value > 0)
+                peso = value;
+        }
+    }
+
+    public Foca(string nome, int idade, string especie, double altura, double peso)
+    {
+        Nome = nome;
+        Idade = idade;
+        Especie = especie;
+        Altura = altura;
+        Peso = peso;
+    }
+
+
+
+    public override string ToString()
+    {
+        return $"Leão-marinho: {Nome} | Espécie: {Especie} | Idade: {Idade} anos | Altura: {Altura} m | Peso: {Peso} kg";
+    }
+
+}
+
--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿LeaoMarinho leao1 = new LeaoMarinho("Béle Belinha", 19, "Australiano", 1.70, 90);
-LeaoMarinho leao2 = new LeaoMarinho("Zé pequno", 12, "Chileno", 1.80, 110);
+LeaoMarinho leao2 = new LeaoMarinho("Zé pequno", 12, "Gálapagos", 1.80, 110);
 
+Foca fofoca1 = new Foca("Felipe", 35, "Foca-Anelada", 1.50, 88 );
 
-leao1.Idade = 6767;
+leao2.Especie = "Argentino";
 
-leao2.Nome = "Dubisgut genu";
+leao2.Nome = "Passa fome";
 
 
 Console.WriteLine(leao1);
